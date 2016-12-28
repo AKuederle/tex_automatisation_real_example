48330b29a9d13b2a391352f3df45e643c3466394
Arne Küderle <a.kuederle@gmail.com>
2016-12-28 15:57:55 +0100

    added templating to main part. Still some issues

--- pycode/data_processing.py
+++ pycode/data_processing.py
@@ -17,10 +17,39 @@ def get_cover_abstract_data(path):
 
     return {'blue_items': blue_items, 'yellow_items': yellow_items, 'abstract_list': abstract_list}
 
+def get_main_data(path):
+    def filter_items(key):
+        items = data[data.index == key]
+        items_dict = items[['item name', 'item alias', 'brief', 'definition']].iloc[0].to_dict()
+        items_dict['subentries'] = list(items.reset_index()[['start', 'icons short list', 'specific description', 'collapse', 'rate']].T.to_dict().values())
+        return items_dict
+
+    def filter_categories(key):
+        items = data[data.category == key]
+        new_keys = data.index.unique()
+        return {k:filter_items(k) for k in new_keys}
+
+    data = pd.read_excel(path, index_col='index')
+    data['icons short list'] = data['icons short list'].str.split(',')
+    data['icons short list'] = data['icons short list'].fillna('[]')
+    data['brief'] = data['brief'].fillna('')
+    data['definition'] = data['definition'].fillna('')
+
+    ### For main
+    categories = data.category.unique()
+    items = data.index.unique()
+    main_list = {k:filter_categories(k) for k in categories}
+
+    return {'main_list': main_list}
+
+
 
 all_variables = {}
 
 cover_abstract_data = get_cover_abstract_data('./cover+abstract.xlsx')
 all_variables = {**all_variables, **cover_abstract_data}
+
+main_data = get_main_data('./main.xlsx')
+all_variables = {**all_variables, **main_data}
 
 compile_pdf_from_template('./template/everything.tex', insert_variables=all_variables, out_path='./out.pdf')
